6ad90476ac2ba7174ceef2892d09cebdb7f6ec8e
ysiouda <ysiouda@erasme.org>
2018-09-20 13:57:21 +0200

    FEAT Displays years and months left

--- left.js
+++ left.js
@@ -1,21 +1,40 @@
+date = new Date();
+
+console.log(date);
+
 // Define dates
 var age = 23;
-var month = 30;
-var years = 80;
+var averageDeath = 80;
+var years = averageDeath - age;
+var months = 12;
 // DIVS
 var divMonth = $("#months");
 var divYears = $("#years");
 
+var monthsLeft =  months - date.getMonth();
+
+console.log(monthsLeft);
 
 $( document ).ready(function() {
-    for (i = 0; i <= month; i++){
+    for (i = 0; i <= months; i++){
         console.log('days');
-        divMonth.append('<div class = "day">DAY</div>');
+        if(i <= monthsLeft){
+            divMonth.append('<div class = "month active"></div>');
+        }
+        else{
+            divMonth.append('<div class = "month"></div>');
+        }
     }
 
-    for (a = 0; a <= years; a++){
+    for (i = 0; i <= averageDeath; i++){
+        if(i <= years){
+            divYears.append('<div class = "year active"></div>');
+        }
+        else {
+            divYears.append('<div class = "year"></div>');
+        }
         console.log('years');
-        divYears.append('<div class = "years">DAY</div>');
+        
     }
 
 });
