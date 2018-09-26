27351817a46debb0f57ab27ba55f0b15dd7f007f
ysiouda <ysiouda@erasme.org>
2018-09-26 14:04:27 +0200

    FEAT Added setting panel

--- left.js
+++ left.js
@@ -4,23 +4,10 @@ $(document).ready(function () {
 
     console.log(date);
 
-    //Define death expectancy
-    var gender = "male";
-    var country = "France";
-
-    //YYYY-MM-DD
-    var dateOfBirth = "1995-06-23";
-
-    //API Call
-    var url = "http://api.population.io:80/1.0/life-expectancy/total/"+gender+"/"+country+"/"+dateOfBirth+"/";
-
-
     // Define dates
     var age = 23;
-    var averageDeath = 80;
-    var years = averageDeath - age;
     var months = 12;
-    
+
     // DIVS
     var divMonth = $("#months");
     var divYears = $("#years");
@@ -29,58 +16,94 @@ $(document).ready(function () {
 
     // Dates maths
     var monthsLeft = months - date.getMonth();
-    var days = getDaysInMonth(date.getMonth()+1, date.getFullYear());
+    var days = getDaysInMonth(date.getMonth() + 1, date.getFullYear());
     var daysLeft = days - date.getDate();
 
     console.log("current month :" + date.getMonth());
     console.log("days in month :" + days);
-    console.log("Resting days in month : "+ daysLeft);
+    console.log("Resting days in month : " + daysLeft);
 
-
-
-    $.getJSON(url, function( data ) {
-        var items = [];
-        $.each( data, function( key, val ) {
-          console.log(val);
-        });
+    // Settings panel
+    $("#open-settings").click(function () {
+        $(".overlay, #settings").fadeIn();
     });
 
-    // Lasting days in a month
-    for (i = 0; i < days; i++) {
-        console.log('days');
-        if (i <= daysLeft) {
-            divDays.append('<div class = "day"></div>');
+    $("#settings .close").click(function () {
+        $(".overlay, #settings").fadeOut();
+    });
+
+    $("#submit").click(function () {
+        console.log("submit");
+        var gender = $("#gender").val();
+        console.log(gender);
+        constructTimeline();
+    })
+
+
+    constructTimeline();
+
+
+
+
+    // Displays all the resting days
+    function constructTimeline(gender= "male", country= "France", dateOfBirth="1990-02-15") {
+
+        //API Call
+        var url = "http://api.population.io:80/1.0/life-expectancy/total/" + gender + "/" + country + "/" + dateOfBirth + "/";
+
+        divDays.empty();
+        divYears.empty();
+        divMonth.empty();
+
+        $.getJSON(url, function (data) {
+            var items = [];
+            $.each(data, function (key, val) {
+                items.push(val);
+            });
+            console.log(items[2]);
+
+            var averageDeath = Math.round(items[2]);
+            var years = averageDeath - age;
+
+            console.log(averageDeath - age);
+
+            // Lasting years based on average death
+            for (i = 0; i < averageDeath; i++) {
+                if (i <= years) {
+                    divYears.append('<div class = "year"></div>');
+                }
+                else {
+                    divYears.append('<div class = "year active"></div>');
+                }
+                console.log('years');
+
+            }
+        });
+
+        // Lasting days in a month
+        for (i = 0; i < days; i++) {
+            console.log('days');
+            if (i <= daysLeft) {
+                divDays.append('<div class = "day"></div>');
+            }
+            else {
+                divDays.append('<div class = "day active"></div>');
+            }
         }
-        else {
-            divDays.append('<div class = "day active"></div>');
+
+        // Lasting months in a year
+        for (i = 0; i < months; i++) {
+            console.log('days');
+            if (i <= monthsLeft) {
+                divMonth.append('<div class = "month"></div>');
+            }
+            else {
+                divMonth.append('<div class = "month active"></div>');
+            }
         }
+
     }
-
-    // Lasting months in a year
-    for (i = 0; i < months; i++) {
-        console.log('days');
-        if (i <= monthsLeft) {
-            divMonth.append('<div class = "month"></div>');
-        }
-        else {
-            divMonth.append('<div class = "month active"></div>');
-        }
-    }
-
-    // Lasting years based on average death
-    for (i = 0; i < averageDeath; i++) {
-        if (i <= years) {
-            divYears.append('<div class = "year"></div>');
-        }
-        else {
-            divYears.append('<div class = "year active"></div>');
-        }
-        console.log('years');
-
-    }
-
 });
-
 
 
 var getDaysInMonth = function (month, year) {
